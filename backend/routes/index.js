4a38a895dba55dab1310b6b435bf28e3c398a285
2024tm93645-manpreetkaur-sasan <2024tm93645@wilp.bits-pilani.ac.in>
2025-11-05 18:57:06 +0530

    Group-13 | Updated the endpoint for equipments

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,5 @@
 const r = require('express').Router();
-r.use('/equipment', require('./equipment'));
+r.use('/equipments', require('./equipment'));
 r.use('/requests', require('./requests'));
 r.use('/notifications', require('./notification'));
 module.exports = r;
